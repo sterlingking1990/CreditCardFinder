cf1e2699495cf9af7ac275b2071a9d289a8adf52
sterlingking1990 <kizundu@seamfix.com>
2021-02-06 08:57:17 +0100

    add other files

--- app/src/main/java/com/project/cardfinder/util/Constants.kt
+++ app/src/main/java/com/project/cardfinder/util/Constants.kt
@@ -1,2 +1,3 @@
 package com.project.cardfinder.util
 
+const val BASE_URL = "https://lookup.binlist.net/"
--- app/src/main/java/com/project/cardfinder/viewmodel/CardInfoViewModel.kt
+++ app/src/main/java/com/project/cardfinder/viewmodel/CardInfoViewModel.kt
@@ -1,4 +1,47 @@
 package com.project.cardfinder.viewmodel
 
-class CardInfoViewModel {
+import android.util.Log
+import android.widget.Toast
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.project.cardfinder.model.response.Card
+import com.project.cardfinder.network.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CardInfoViewModel @ViewModelInject constructor(var cardApi:ApiService): ViewModel(){
+
+    private var _cardInfo = MutableLiveData<Card>()
+    val cardInfo: LiveData<Card> get() = _cardInfo
+
+    private var _success = MutableLiveData<Boolean>()
+    val success:LiveData<Boolean> get() = _success
+
+    private lateinit var iin:Card
+
+
+
+    fun getCardInfo(iin:String){
+        Log.d("iin",iin.toString())
+        val cardInfo = cardApi.api.getCardInfo(iin)
+        cardInfo.enqueue(
+            object : Callback<Card> {
+                override fun onFailure(call: Call<Card>, t: Throwable) {
+                    _success.value = false
+                    Log.d("FAILURE", t.message.toString())
+                }
+
+                override fun onResponse(call: Call<Card>, response: Response<Card>) {
+                    _success.value = true
+                    _cardInfo.value = response.body()
+                    Log.d("SUCCESS", cardInfo.toString())
+                }
+            }
+        )
+    }
 }
