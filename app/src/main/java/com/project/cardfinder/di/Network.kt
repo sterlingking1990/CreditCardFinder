25cd44a486c981ccdb4fe01657238f1ecc47faba
sterlingking1990 <kizundu@seamfix.com>
2021-02-06 08:53:27 +0100

    set up dependency injection

---
+++ app/src/main/java/com/project/cardfinder/di/Network.kt
@@ -0,0 +1,54 @@
+package com.project.cardfinder.di
+
+import com.project.cardfinder.network.API
+import com.project.cardfinder.network.repository.CardInfoRepository
+import com.project.cardfinder.network.repository.CardInfoRepositoryImpl
+import com.project.cardfinder.util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class Network {
+
+    @Provides
+    @Singleton
+    fun providesConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(client: OkHttpClient, converterFactory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(converterFactory)
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): API {
+        return retrofit.create(API::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesCardInfoRepository(cardInfoRepositoryImpl: CardInfoRepositoryImpl): CardInfoRepository {
+        return cardInfoRepositoryImpl
+    }
+}
