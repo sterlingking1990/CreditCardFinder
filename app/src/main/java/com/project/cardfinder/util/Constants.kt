b7173befd5077b625a5f420b6b43668ae3c8b54e
sterlingking1990 <kizundu@seamfix.com>
2021-02-07 21:20:57 +0100

    add constant for network or no input error

--- app/src/main/java/com/project/cardfinder/util/Constants.kt
+++ app/src/main/java/com/project/cardfinder/util/Constants.kt
@@ -1,3 +1,5 @@
 package com.project.cardfinder.util
 
 const val BASE_URL = "https://lookup.binlist.net/"
+const val UNABLE_TO_ESTABLISH_CONNECTION = "could not find card due to no input or network connection"
+const val SPLASH_TIME_OUT = 3000L
